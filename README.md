7c5874617ffbaa8fa929269230e540e533925ff4
NobpasinTumdee <porporpor579@gmail.com>
2025-08-14 01:06:58 +0700

    connected supabase db

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ npm create vite@latest
 ```
 npm i react-router-dom
 ```
+```
+npm install @supabase/supabase-js
+```
+```
+npm install react-dotenv
+```
--- quick-task/src/page/Home.tsx
+++ quick-task/src/page/Home.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { supabase } from "../supabase/supabaseClient";
+
+export type users = {
+    id: number;
+    name: string;
+    password: string;
+    created_at: string;
+}
 
 const Home = () => {
+    const [users, setUsers] = useState<users[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchUsers = async () => {
+        setLoading(true);
+        const { data, error } = await supabase.from('users').select('*');
+        setLoading(false);
+        if (!error) {
+            setUsers(data);
+            console.log("Fetched users:", data);
+        } else {
+            console.error("Error fetching users:", error);
+        }
+    };
+
     return (
         <>
             <h1>Home</h1>
+            {users.map((user) => (
+                <div key={user.id}>
+                    <p>{user.name}</p>
+                    <p>{user.created_at}</p>
+                    <p>{user.password}</p>
+                </div>
+            ))}
+            {loading ? <p>Loading...</p> : <button onClick={fetchUsers}>Get Users</button>}
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
+
