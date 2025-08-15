aa449fc511ee98632265edadc541b70c2be7891d
NobpasinTumdee <porporpor579@gmail.com>
2025-08-15 20:54:03 +0700

    Home ui

--- quick-task/src/page/Home.tsx
+++ quick-task/src/page/Home.tsx
@@ -1,10 +1,33 @@
+import { Link } from 'react-router-dom';
 import '../App.css'
-
+import './style/Home.css'
 
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <div className="hero-container">
+                {/* Navigation */}
+                <nav className="navigation">
+                    <div className="nav-brand">
+                        <div className="nav-logo">
+                            ✨
+                        </div>
+                        <span className="nav-title">Quick Task</span>
+                    </div>
+
+                    <div className="nav-links">
+                        <Link to="/task" className="nav-link">Task Manager</Link>
+                        <Link to="/todo-list" className="nav-link">Todo list</Link>
+                        <Link to="/me" className="nav-link">About</Link>
+                        <Link to="/login" className="nav-button">Sign In</Link>
+                    </div>
+
+                </nav>
+                <div className='hero-content'>
+                    <h1>Organize Your Life<br />Effortlessly</h1>
+                    <h3>Master your daily routine with our all-in-one productivity platform. Manage tasks, track finances, and achieve your goals with intelligent automation.</h3>
+                </div>
+            </div>
         </>
     );
 };
