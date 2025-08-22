e24e08b9946ca9881a4e532ad02e988912515ed7
NobpasinTumdee <porporpor579@gmail.com>
2025-08-22 22:42:13 +0700

    update ui

--- quick-task/src/page/Finance/Finance.tsx
+++ quick-task/src/page/Finance/Finance.tsx
@@ -299,7 +299,7 @@ const Finance = () => {
                                     <p style={{ opacity: 0.7 }}>{String(transaction.transaction_date).slice(0, 10)}</p>
                                 </div>
                                 {lazyLoading ? (
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#8d8d8dff">
+                                    <svg className="animation-clock" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#8d8d8dff">
                                         <path d="M480-520q66 0 113-47t47-113v-120H320v120q0 66 47 113t113 47ZM160-80v-80h80v-120q0-61 28.5-114.5T348-480q-51-32-79.5-85.5T240-680v-120h-80v-80h640v80h-80v120q0 61-28.5 114.5T612-480q51 32 79.5 85.5T720-280v120h80v80H160Z" />
                                     </svg>
                                 ) : (
