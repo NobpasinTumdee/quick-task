9430f3692d6bca39a2ab23cb7d464611b499218e
NobpasinTumdee <porporpor579@gmail.com>
2025-08-27 16:36:26 +0700

    เพิ่มน้อง Quick เข้ามาในเว็ป

--- quick-task/src/page/Home.tsx
+++ quick-task/src/page/Home.tsx
@@ -5,6 +5,10 @@ import '../App.css'
 import './style/Home.css'
 import { useEffect } from 'react';
 import a3 from '../assets/logo/a3.png'
+import Quick1 from '../assets/logo/a1.png';
+import Quick2 from '../assets/logo/a2.png';
+import Quick3 from '../assets/logo/a3.png';
+import Quick4 from '../assets/logo/a4.png';
 
 const Home = () => {
     useEffect(() => { Aos.init({ duration: 1000, once: true, }); }, []);
@@ -31,6 +35,12 @@ const Home = () => {
                     <h1 data-aos="zoom-in-up" data-aos-duration="1500">Organize Your Life<br />Effortlessly</h1>
                     <h3 data-aos="zoom-in-up" data-aos-duration="2500">Master your daily routine with our all-in-one productivity platform. Manage tasks, track finances, and achieve your goals with intelligent automation.</h3>
                 </div>
+            </div>
+            <div className="quick-home" data-aos="fade-up" data-aos-duration="4000">
+                <img src={Quick3} className="quick-home3" />
+                <img src={Quick1} className="quick-home1" />
+                <img src={Quick4} className="quick-home4" />
+                <img src={Quick2} className="quick-home2" />
             </div>
         </>
     );
